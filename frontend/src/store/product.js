90484a0dba89dfa741db22c40ce63b713d0e2183
kevin young <you.k2208@gmail.com>
2024-09-24 15:38:01 -0700

    fixed formatting errors in vite config

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,11 +1,11 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
   createProduct: async (newProduct) => {
-    if(!newProduct.name || !newProduct.price || !newProduct.image) {
-      return {success: false, message: "Please fill in all fields."}
+    if (!newProduct.name || !newProduct.price || !newProduct.image) {
+      return { success: false, message: "Please fill in all fields." };
     }
     const res = await fetch("/api/products", {
       method: "POST",
