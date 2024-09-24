d75191ccf293cff1b9fd29c13009da9c9d653768
kevin young <you.k2208@gmail.com>
2024-09-23 20:29:51 -0700

    finished get all http method for products endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,17 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the req.body - middleware
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Eror" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
 
@@ -33,6 +44,7 @@ app.delete("/api/products/:id", async (req, res) => {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
+    console.log("error in deleting product:", error.message);
     res.status(404).json({ success: false, message: "Product not found" });
   }
 });
