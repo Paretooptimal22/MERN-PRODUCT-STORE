f9809f7d485535095e70febf82acaee8d1c97914
kevin young <you.k2208@gmail.com>
2024-09-24 12:26:27 -0700

    updated dotenv, server, and product controller

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); //allows us to accept JSON data in the req.body - middleware
 
 app.use("/api/products", productRoutes);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5001");
+  console.log("Server started at http://localhost:" + PORT);
 });
