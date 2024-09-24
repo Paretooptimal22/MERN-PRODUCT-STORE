33be278815ba713efe0db45592397b9b078bb767
kevin young <you.k2208@gmail.com>
2024-09-24 16:15:54 -0700

    created fetchProducts function in global state and added useEffect to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
-import { Container, Text, VStack } from "@chakra-ui/react";
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+
   return (
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
@@ -14,6 +23,18 @@ const HomePage = () => {
 				>
 					Current Products
 				</Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+
+        </SimpleGrid>
 
         <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
 						No products found{" "}
