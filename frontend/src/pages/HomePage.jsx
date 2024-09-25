55e55200357db0a41ddcbc1224dcf1dee74dafc4
kevin young <you.k2208@gmail.com>
2024-09-24 17:06:58 -0700

    added if statement so text only shows if product length is zero

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -36,17 +36,19 @@ const HomePage = () => {
         >
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
-          ))}
+          ))};
         </SimpleGrid>
 
-        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-						No products found{" "}
-						<Link to={"/create"}>
-							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-								Create a product
-							</Text>
-						</Link>
-					</Text>
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+					  No products found{" "}
+            <Link to={"/create"}>
+              <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+                Create a product
+              </Text>
+            </Link>
+				  </Text>
+        )};
       </VStack>
     </Container>
   )
